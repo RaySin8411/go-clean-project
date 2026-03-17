0874e705c5f50aae8313d635243f2ce8fb0095ba
raysin8411 <q8977452@gmail.com>
2026-03-17 10:13:29 +0800

    (develop) Database

--- internal/application/application.go
+++ internal/application/application.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 
 	"go-clean-project/internal/config"
+	"go-clean-project/internal/database/mysql"
 	"go-clean-project/internal/logger"
 )
 
 type Application struct {
-	Config *config.Config
-	Logger logger.Logger
+	Config   *config.Config
+	Logger   logger.Logger
+	Database *mysql.Database
 
 	Service *Service
 	UseCase *UseCase
@@ -19,10 +21,15 @@ type Application struct {
 func New(cfg *config.Config) (*Application, error) {
 	ctx := context.Background()
 	logger := logger.NewSLogger(ctx, cfg.Logger)
+	database, err := mysql.InitDatabase(cfg.MySQL.DSN(), logger.GetDatabaseLogger())
+	if err != nil {
+		return nil, err
+	}
 
 	app := &Application{
-		Config: cfg,
-		Logger: logger,
+		Config:   cfg,
+		Logger:   logger,
+		Database: database,
 	}
 
 	service, err := NewService(app)
